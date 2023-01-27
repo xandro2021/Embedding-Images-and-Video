058574e9fcee428190ad1bdb3ead5d27cbc69374
xandro2021 <xandrocastro2021@gmail.com>
2023-01-26 19:42:45 -0600

    Moving the files

--- JavaScript/ExercisesPart2/Challenge03-Stair-Case/stairCase.js
+++
@@ -1,8 +0,0 @@
-function staircase(n) {
-  // Write your code here
-  for (let i = 1; i <= n; i++) {
-    console.log( " ".repeat( n - i ) + "#".repeat( i ) );
-  }
-}
-
-staircase( 10 );
--- JavaScript/ExercisesPart2/Challenge08-The-Hurdle-Race/theHurdleRace.js
+++
@@ -1,7 +0,0 @@
-function hurdleRace(k, height) {
-  // Write your code here
-  let maxValue = height.reduce((max, currentValue) => (currentValue > max) ? currentValue: max);
-  return ( maxValue - k > 0 ) ? ( maxValue - k ) : 0;
-}
-
-console.log( hurdleRace( 7, [2, 5, 4, 5, 2] ) );
